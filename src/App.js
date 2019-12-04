863b64cbf43ebf2dd57ea285253ec37c83820af4
Schrese <53787633+Schrese@users.noreply.github.com>
2019-12-04 18:40:31 -0500

    moved pages around and added routes

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+//components
+import MainPage from './components/MainPage';
+import Details from './components/Details';
+
+function App() {
+
+    return(
+        <div>
+            <Switch>
+                <Route exact path='/' component={MainPage} />
+                <Route path = '/Details' component={Details} />
+            </Switch>
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,11 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
-import {Route, Switch} from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import './styles.scss';
 
-import Charts from "./components/Charts";
-import Navbar from "./components/Navbar";
-import Details from './components/Details';
-
-import "./styles.scss";
-
-const App = () => {
-  const [coinData, setCoinData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => {
-        console.log(res)
-        setCoinData(res.data)})
-      .catch(err => console.log(err));
-  }, []);
-  return (
-    <div className="App">
-      <Navbar />
-      <Charts coinData={coinData} />
-      {/* <Details coinData = {coinData} /> */}
-    </div>
-  );
-};
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+, document.getElementById('root'));
