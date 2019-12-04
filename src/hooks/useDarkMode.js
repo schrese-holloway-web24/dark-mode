26d38bd1e4b0952ef22ff2aba403ac535db3ac61
Schrese <53787633+Schrese@users.noreply.github.com>
2019-12-04 15:55:07 -0500

    Met MVP, dark mode persists

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,15 @@
 import {useLocalStorage} from './useLocalStorage';
+import {useEffect} from 'react';
 
-const useDarkMode = () => {
-    useLocalStorage()
+export const useDarkMode = (key, initialValue) => {
+    const [dark, setDark] = useLocalStorage(key, initialValue)
+
+    useEffect(() => {
+        if(dark === true) {
+            document.querySelector('body').classList.add('dark-mode')
+        } else {
+            document.querySelector('body').classList.remove('dark-mode')
+        }
+    }, [dark])
+    return [dark, setDark]
 }
